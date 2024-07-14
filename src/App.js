0643ddb56c533629450b950ce28c6408e0952388
Sarthak Kumar <famousfive.17@gmail.com>
2024-07-14 22:32:21 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Logo from "../src/assets/Logo.svg";
 import SongNav from "./components/SongNav";
 import SongList from "./components/SongList";
 import Mp3 from "./components/Mp3";
+import { FaBars } from "react-icons/fa";
 
 function App() {
   const [songs, setSongs] = useState([]);
@@ -12,7 +13,8 @@ function App() {
   const [showTopTracks, setShowTopTracks] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [duration, setDuration] = useState(0);
-  const [currentPlayingSongId, setCurrentPlayingSongId] = useState(null); // State to track currently playing song ID
+  const [currentPlayingSongId, setCurrentPlayingSongId] = useState(null);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const backgroundStyle = selectedSong
     ? { background: `linear-gradient(to right, ${selectedSong.accent}, black)` }
@@ -32,59 +34,66 @@ function App() {
     const index = songs.findIndex((s) => s.id === song.id);
     setCurrentSongIndex(index);
     setSelectedSong(song);
-    setCurrentPlayingSongId(song.id); // Update currently playing song ID
+    setCurrentPlayingSongId(song.id);
   };
 
   const handleNextSong = () => {
     const nextIndex = (currentSongIndex + 1) % songs.length;
     setCurrentSongIndex(nextIndex);
     setSelectedSong(songs[nextIndex]);
-    setCurrentPlayingSongId(songs[nextIndex].id); // Update currently playing song ID
+    setCurrentPlayingSongId(songs[nextIndex].id);
   };
 
   const handlePreviousSong = () => {
     const prevIndex = (currentSongIndex - 1 + songs.length) % songs.length;
     setCurrentSongIndex(prevIndex);
     setSelectedSong(songs[prevIndex]);
-    setCurrentPlayingSongId(songs[prevIndex].id); // Update currently playing song ID
+    setCurrentPlayingSongId(songs[prevIndex].id);
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-300 flex-row">
+    <div className="flex justify-center items-center min-h-screen bg-gray-300 flex-col md:flex-row">
       <div
-        className="dynamic-bg w-[1512px] h-[895px] flex flex-row"
+        className="dynamic-bg w-full min-h-screen flex flex-col md:flex-row"
         style={backgroundStyle}
       >
-        <div className="mr-[130px] mt-[32px] ml-[32px]">
-          <img src={Logo} alt="" className="w-[133.41px] h-[40px]" />
+        <div className="p-4">
+          <img src={Logo} alt="" className="w-32 h-10" />
         </div>
-        <div className="flex flex-row">
-          <div className="flex flex-col w-[432px] h-full items-center">
-            <div className="mt-[32px]">
+        <div className="flex flex-col md:flex-row w-full">
+          <div className="flex flex-col w-full md:w-1/3 items-center p-4">
+            <div className="block md:hidden">
+              <FaBars
+                className="text-white text-2xl cursor-pointer"
+                onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+              />
+            </div>
+            <div
+              className={`w-full mt-[40px] ml-[400px] ${
+                isMobileMenuOpen ? "block" : "hidden"
+              } md:block`}
+            >
               <SongNav
                 setShowTopTracks={setShowTopTracks}
                 showTopTracks={showTopTracks}
                 searchQuery={searchQuery}
                 setSearchQuery={setSearchQuery}
               />
-            </div>
-            <div className="flex justify-start mt-[32px]">
               <SongList
                 songs={songs}
                 onSelectSong={handleSelectSong}
                 showTopTracks={showTopTracks}
                 searchQuery={searchQuery}
-                currentPlayingSongId={currentPlayingSongId} // Pass currently playing song ID
+                currentPlayingSongId={currentPlayingSongId}
               />
             </div>
           </div>
-          <div>
+          <div className="flex-1 flex justify-center items-center p-4 ">
             <Mp3
               selectedSong={selectedSong}
               setDuration={setDuration}
               onNext={handleNextSong}
               onPrev={handlePreviousSong}
-              setCurrentPlayingSongId={setCurrentPlayingSongId} // Pass function to update currently playing song ID
             />
           </div>
         </div>
